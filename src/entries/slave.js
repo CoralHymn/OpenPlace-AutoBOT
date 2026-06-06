5f9a97d885730f7aef41ed05c755939a7a9dd009
coralhymn <220769249+CoralHymn@users.noreply.github.com>
2026-06-06 23:51:23 +0800

    移除无关文件

--- src/entries/slave.js
+++
@@ -1,7 +0,0 @@
-// Entry point para Auto-Slave
-import { runSlave } from '../slave/index.js';
-
-// Ejecutar el slave automáticamente
-runSlave().catch(error => {
-  console.error('[Auto-Slave] Error fatal:', error);
-});
--- src/image/progress.js
+++
@@ -1,4 +0,0 @@
-import { load, save } from "../core/storage.js";
-
-export const loadProgress = () => load("image:progress", null);
-export const saveProgress = (p) => save("image:progress", p);
